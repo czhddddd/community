702c025c1682dc7a355f63fb9a9dd5e7dc5c3ab4
czh <lijinchenzihao@163.com>
2020-04-28 11:36:15 +0800

    解决pom问题

--- src/main/java/com/czh/communityApplication.java
+++ src/main/java/com/czh/communityApplication.java
@@ -2,10 +2,13 @@ package com.czh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 public class communityApplication {
     public static void main(String[] args) {
         SpringApplication.run(communityApplication.class,args);
     }
+
 }
